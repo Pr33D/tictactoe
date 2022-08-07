1bfdfc55bf8c904f3f47edd12d720e0050836344
Pr33D <keymaps@magenta.de>
2022-08-07 20:54:10 +0200

    switched to scss & added gameBoard

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const Gameboard = (() => {
-let gameBoard = ["x", "", "o", "x", "", "", "o", "", ""];
-gameBoard.forEach(e => {
-    const fieldButton = document.createElement('div');
-    fieldButton.setAttribute("id", e);
+  let gameBoard = ["x", "", "o", "x", "", "", "o", "", ""];
+  gameBoard.forEach((e, index) => {
+    const fieldButton = document.createElement("div");
+    fieldButton.classList.add("fieldButton");
+    fieldButton.setAttribute("id", index);
+    fieldButton.innerHTML = e;
     document.querySelector("#field").appendChild(fieldButton);
-});
+  });
 })();
 
-const playGame = (() => {
-
-})();
+const playGame = (() => {})();
 
 const Player = (name, xo) => {
-    return {name, xo};
+  return { name, xo };
 };
